196797931214868e953dff45fc89c868307bbcb7
Suhesh Kasti <suhesh.kasti@digitalnetwork.com.np>
2025-06-10 12:34:54 +0545

    Worker Test

---
+++ build.sh
@@ -0,0 +1,35 @@
+main() {
+  HUGO_VERSION=0.147.7
+
+  export TZ=Kathmandu/Asia
+
+  # Install Hugo
+  echo "Installing Hugo v${HUGO_VERSION}..."
+  curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+  tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  cp hugo /opt/buildhome
+  rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+
+  # Set PATH
+  echo "Setting the PATH environment variable..."
+  export PATH=/opt/buildhome:$PATH
+
+  # Verify installed versions
+  echo "Verifying installations..."
+  echo Go: "$(go version)"
+  echo Hugo: "$(hugo version)"
+  echo Node.js: "$(node --version)"
+
+  # https://gohugo.io/methods/page/gitinfo/#hosting-considerations
+  git fetch --recurse-submodules --unshallow
+
+  # https://github.com/gohugoio/hugo/issues/9810
+  git config core.quotepath false
+
+  # Build the site.
+  npm run build
+
+}
+
+set -euo pipefail
+main "$@"
